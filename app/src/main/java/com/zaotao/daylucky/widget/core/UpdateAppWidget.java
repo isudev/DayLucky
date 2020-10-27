a48129156f330384b93c08d339c2920fc09d4e77
wangisu <wangisu@qq.com>
2020-10-27 18:10:50 +0800

    merge update app widget remote views

--- app/src/main/java/com/zaotao/daylucky/widget/core/UpdateAppWidget.java
+++ app/src/main/java/com/zaotao/daylucky/widget/core/UpdateAppWidget.java
@@ -34,45 +34,19 @@ public class UpdateAppWidget {
 
     public void updateAppWidget() {
         ComponentName componentName = new ComponentName(App.getApplication(), DayLuckyAppWidget.class);
-        RemoteViews views = new RemoteViews(App.getApplication().getPackageName(), R.layout.day_lucky_app_widget);
         AppWidgetManager awm = AppWidgetManager.getInstance(App.getApplication());
-        ThemeEntity themeEntity = LocalDataManager.getInstance().getThemeData();
-
-        views.setTextViewText(R.id.item_theme_style_day_text, themeEntity.getDay());
-        views.setTextViewText(R.id.item_theme_style_month_text, themeEntity.getMonth());
-        views.setTextViewText(R.id.item_theme_style_week_text, themeEntity.getWeek());
-        views.setTextViewText(R.id.item_theme_style_content_text, themeEntity.getText());
-        views.setTextViewText(R.id.item_theme_style_day_lucky_text, themeEntity.getLucky());
-        views.setTextViewText(R.id.item_theme_style_day_bad_text, themeEntity.getBad());
-
-        views.setTextColor(R.id.item_theme_style_day_text, themeEntity.getDayColor());
-        views.setTextColor(R.id.item_theme_style_week_text, themeEntity.getWeekColor());
-        views.setTextColor(R.id.item_theme_style_month_text, themeEntity.getMonthColor());
-        views.setTextColor(R.id.item_theme_style_day_lucky_text, themeEntity.getLuckyColor());
-        views.setTextColor(R.id.item_theme_style_day_bad_text, themeEntity.getBadColor());
-        views.setTextColor(R.id.item_theme_style_content_text, themeEntity.getTextColor());
-
-        if (themeEntity.getBgColor() == ColorManager.normalWhiteColor) {
-            views.setTextColor(R.id.item_theme_style_day_lucky, ColorManager.colorTextView);
-            views.setTextColor(R.id.item_theme_style_day_bad, ColorManager.colorTextView);
-        } else {
-            views.setTextColor(R.id.item_theme_style_day_lucky, ColorManager.normalWhiteColor);
-            views.setTextColor(R.id.item_theme_style_day_bad, ColorManager.normalWhiteColor);
-        }
-
-        Bitmap bitmap0 = Bitmap.createBitmap(6, 80, Bitmap.Config.ARGB_8888);
-        bitmap0.eraseColor(themeEntity.getLineColor());
-        views.setImageViewBitmap(R.id.item_theme_style_line, bitmap0);
-        Bitmap bitmap = getBitmap(ScreenUtils.getScreenWidth(), 400, themeEntity.getBgColor());
-        views.setImageViewBitmap(R.id.item_theme_style_bg, bitmap);
-
-        awm.updateAppWidget(componentName, views);
+        awm.updateAppWidget(componentName, getRemoteViews(App.getApplication().getPackageName()));
     }
 
     public void updateAppWidget(Context context, AppWidgetManager appWidgetManager, int appWidgetId) {
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, getRemoteViews(context.getPackageName()));
+    }
+
+    private RemoteViews getRemoteViews(String packageName){
         ThemeEntity themeEntity = LocalDataManager.getInstance().getThemeData();
         // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.day_lucky_app_widget);
+        RemoteViews views = new RemoteViews(packageName, R.layout.day_lucky_app_widget);
         views.setTextViewText(R.id.item_theme_style_day_text, themeEntity.getDay());
         views.setTextViewText(R.id.item_theme_style_month_text, themeEntity.getMonth());
         views.setTextViewText(R.id.item_theme_style_week_text, themeEntity.getWeek());
@@ -100,8 +74,8 @@ public class UpdateAppWidget {
         views.setImageViewBitmap(R.id.item_theme_style_line, bitmap0);
         Bitmap bitmap = getBitmap(ScreenUtils.getScreenWidth(), 400, themeEntity.getBgColor());
         views.setImageViewBitmap(R.id.item_theme_style_bg, bitmap);
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
+
+        return views;
     }
 
     private static Bitmap getBitmap(int width, int height, int color) {
