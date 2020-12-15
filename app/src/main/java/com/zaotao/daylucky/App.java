ebc0aac83b5d5cd2960092f53e122500d297b854
wangisu <wangisu@qq.com>
2020-12-15 15:19:23 +0800

    chagne unmeng libs

--- app/src/main/java/com/zaotao/daylucky/App.java
+++ app/src/main/java/com/zaotao/daylucky/App.java
@@ -1,6 +1,13 @@
 package com.zaotao.daylucky;
 
+import android.app.Activity;
 import android.app.Application;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.umeng.analytics.MobclickAgent;
 import com.umeng.commonsdk.UMConfigure;
@@ -21,8 +28,43 @@ public class App extends Application {
         //init base module
         Utils.init(this);
         //umeng
-        UMConfigure.init(this, UMConfigure.DEVICE_TYPE_PHONE, "3b3fa31a954b4ce5415474cc22dabd03");
+        UMConfigure.init(this, "5fd8612f498d9e0d4d8f3c86", Build.BRAND, UMConfigure.DEVICE_TYPE_PHONE, "");
+        UMConfigure.setLogEnabled(true);
         MobclickAgent.setPageCollectionMode(MobclickAgent.PageMode.AUTO);
-    }
+        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+            @Override
+            public void onActivityCreated(@NonNull Activity activity, @Nullable Bundle savedInstanceState) {
+            }
 
+            @Override
+            public void onActivityStarted(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivityResumed(@NonNull Activity activity) {
+                MobclickAgent.onResume(activity);
+            }
+
+            @Override
+            public void onActivityPaused(@NonNull Activity activity) {
+                MobclickAgent.onPause(activity);
+            }
+
+            @Override
+            public void onActivityStopped(@NonNull Activity activity) {
+
+            }
+
+            @Override
+            public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle outState) {
+
+            }
+
+            @Override
+            public void onActivityDestroyed(@NonNull Activity activity) {
+
+            }
+        });
+    }
 }
--- app/src/main/java/com/zaotao/daylucky/base/BaseActivity.java
+++ app/src/main/java/com/zaotao/daylucky/base/BaseActivity.java
@@ -16,7 +16,6 @@ import androidx.annotation.LayoutRes;
 import androidx.annotation.Nullable;
 
 import com.trello.rxlifecycle3.components.support.RxAppCompatActivity;
-import com.umeng.message.PushAgent;
 
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
@@ -72,8 +71,6 @@ public abstract class BaseActivity<T extends BasePresenter> extends RxAppCompatA
         initCreate(savedInstanceState);
         //初始化监听
         initListener();
-        //该方法是【友盟+】Push后台进行日活统计及多维度推送的必调用方法
-        PushAgent.getInstance(mContext).onAppStart();
     }
 
     /**
