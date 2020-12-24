a369af263bdf62c6ed14c4efccf6580814cae39b
wangisu <wangisu@qq.com>
2020-12-24 19:54:04 +0800

    add push libs

--- app/src/main/java/com/zaotao/daylucky/App.java
+++ app/src/main/java/com/zaotao/daylucky/App.java
@@ -2,19 +2,29 @@ package com.zaotao.daylucky;
 
 import android.app.Activity;
 import android.app.Application;
+import android.app.Notification;
+import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.RemoteViews;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import com.umeng.analytics.MobclickAgent;
 import com.umeng.commonsdk.UMConfigure;
+import com.umeng.message.IUmengRegisterCallback;
+import com.umeng.message.MsgConstant;
+import com.umeng.message.PushAgent;
+import com.umeng.message.UmengMessageHandler;
+import com.umeng.message.UmengNotificationClickHandler;
+import com.umeng.message.entity.UMessage;
 import com.zaotao.base.utils.Utils;
 
 public class App extends Application {
 
+    private static final String TAG = "DayLuckyApp";
     private static Application app;
 
     public static Application getApplication() {
@@ -31,12 +41,13 @@ public class App extends Application {
         String brand = Build.BRAND.toLowerCase();
         brand = brand.equals("unknown") ? "other" : brand;
         Log.i("DayLucky", "brand: " + brand);
-        UMConfigure.init(this, "5fd8612f498d9e0d4d8f3c86", brand, UMConfigure.DEVICE_TYPE_PHONE, "");
+        UMConfigure.init(this, "5fd8612f498d9e0d4d8f3c86", brand, UMConfigure.DEVICE_TYPE_PHONE, "7458c44800e5aa1c99fe73f8f09284a9");
         UMConfigure.setLogEnabled(true);
         MobclickAgent.setPageCollectionMode(MobclickAgent.PageMode.AUTO);
         registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
             @Override
             public void onActivityCreated(@NonNull Activity activity, @Nullable Bundle savedInstanceState) {
+                PushAgent.getInstance(activity).onAppStart();
             }
 
             @Override
@@ -69,5 +80,133 @@ public class App extends Application {
 
             }
         });
+
+        initPush(this);
     }
+
+    public void initPush(Context context) {
+        PushAgent mPushAgent = PushAgent.getInstance(context);
+        mPushAgent.register(new IUmengRegisterCallback() {
+            @Override
+            public void onSuccess(String deviceToken) {
+                Log.i(TAG, "onSuccess: "+deviceToken);
+//                UserInfoManager.getInstance().setDeviceToken(deviceToken);
+            }
+
+            @Override
+            public void onFailure(String s, String s1) {
+            }
+        });
+        //sdk开启通知声音
+//        mPushAgent.setNotificationPlaySound(MsgConstant.NOTIFICATION_PLAY_SDK_ENABLE);
+        // sdk关闭通知声音
+        // mPushAgent.setNotificationPlaySound(MsgConstant.NOTIFICATION_PLAY_SDK_DISABLE);
+        // 通知声音由服务端控制
+        mPushAgent.setNotificationPlaySound(MsgConstant.NOTIFICATION_PLAY_SERVER);
+
+//         mPushAgent.setNotificationPlayLights(MsgConstant.NOTIFICATION_PLAY_SDK_DISABLE);
+//         mPushAgent.setNotificationPlayVibrate(MsgConstant.NOTIFICATION_PLAY_SDK_DISABLE);
+
+        UmengMessageHandler messageHandler = new UmengMessageHandler() {
+
+            /**
+             * 通知的回调方法（通知送达时会回调）
+             */
+            @Override
+            public void dealWithNotificationMessage(Context context, UMessage msg) {
+                //调用super，会展示通知，不调用super，则不展示通知。
+                super.dealWithNotificationMessage(context, msg);
+            }
+
+            /**
+             * 自定义消息的回调方法
+             */
+            @Override
+            public void dealWithCustomMessage(final Context context, final UMessage msg) {
+
+                /**
+                 *  boolean isClickOrDismissed = true;
+                 *                         if (isClickOrDismissed) {
+                 *                             //自定义消息的点击统计
+                 *                             UTrack.getInstance(context).trackMsgClick(msg);
+                 *                         } else {
+                 *                             //自定义消息的忽略统计
+                 *                             UTrack.getInstance(context).trackMsgDismissed(msg);
+                 *                         }
+                 *
+                 */
+
+
+            }
+
+
+            /**
+             * 自定义通知栏样式的回调方法
+             */
+            @Override
+            public Notification getNotification(Context context, UMessage msg) {
+                switch (msg.builder_id) {
+                    case 1:
+                        Notification.Builder builder = new Notification.Builder(context);
+                        RemoteViews myNotificationView = new RemoteViews(context.getPackageName(),
+                                R.layout.app_notification_view);
+                        myNotificationView.setTextViewText(R.id.notification_title, msg.title);
+                        myNotificationView.setTextViewText(R.id.notification_text, msg.text);
+                        myNotificationView.setImageViewBitmap(R.id.notification_large_icon, getLargeIcon(context, msg));
+                        myNotificationView.setImageViewResource(R.id.notification_small_icon,
+                                getSmallIconId(context, msg));
+                        builder.setContent(myNotificationView)
+                                .setSmallIcon(getSmallIconId(context, msg))
+                                .setTicker(msg.ticker)
+                                .setAutoCancel(true);
+
+                        return builder.getNotification();
+                    default:
+                        //默认为0，若填写的builder_id并不存在，也使用默认。
+                        return super.getNotification(context, msg);
+                }
+            }
+        };
+        mPushAgent.setMessageHandler(messageHandler);
+
+        /**
+         * 自定义行为的回调处理，参考文档：高级功能-通知的展示及提醒-自定义通知打开动作
+         * UmengNotificationClickHandler是在BroadcastReceiver中被调用，故
+         * 如果需启动Activity，需添加Intent.FLAG_ACTIVITY_NEW_TASK
+         * */
+        UmengNotificationClickHandler notificationClickHandler = new UmengNotificationClickHandler() {
+
+            @Override
+            public void launchApp(Context context, UMessage msg) {
+                String type = msg.extra.get("type");
+
+
+                super.launchApp(context, msg);
+            }
+
+            @Override
+            public void openUrl(Context context, UMessage msg) {
+                super.openUrl(context, msg);
+            }
+
+            @Override
+            public void openActivity(Context context, UMessage msg) {
+                super.openActivity(context, msg);
+            }
+        };
+        //使用自定义的NotificationHandler
+        mPushAgent.setNotificationClickHandler(notificationClickHandler);
+
+
+        //使用完全自定义处理
+        //mPushAgent.setPushIntentServiceClass(UmengNotificationService.class);
+
+        //小米通道
+        //MiPushRegistar.register(this, XIAOMI_ID, XIAOMI_KEY);
+        //华为通道
+        //HuaWeiRegister.register(this);
+        //魅族通道
+        //MeizuRegister.register(this, MEIZU_APPID, MEIZU_APPKEY);
+    }
+
 }
